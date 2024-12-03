da09882e947fd2c25289295c91b64e474e8b259b
easin1740 <hossan2305101740@diu,edu.bd>
2024-12-04 00:13:27 +0600

    15th commit

--- Mx/app/src/main/java/com/m/x/AIDEApplication.java
+++ Mx/app/src/main/java/com/m/x/AIDEApplication.java
@@ -8,3 +8,10 @@ import com.google.android.gms.ads.MobileAds;
 public class AIDEApplication extends Application
 {
 	private Thread.UncaughtExceptionHandler uncaughtExceptionHandler;
+	@Override
+	public void onCreate() {
+		MobileAds.initialize(this, "ca-app-pub-3940256099942544~3347511713");
+		this.uncaughtExceptionHandler = Thread.getDefaultUncaughtExceptionHandler();
+
+		Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+			@Override
