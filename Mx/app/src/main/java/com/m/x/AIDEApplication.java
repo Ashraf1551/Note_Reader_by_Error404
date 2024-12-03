f58d0d7b7e61431a3935391b47c5817cf2278bc8
easin1740 <hossan2305101740@diu,edu.bd>
2024-12-04 00:01:52 +0600

    14th

--- Mx/app/src/main/java/com/m/x/AIDEApplication.java
+++ Mx/app/src/main/java/com/m/x/AIDEApplication.java
@@ -5,3 +5,6 @@ import java.io.*;
 import android.content.Context;
 import android.content.Intent;
 import com.google.android.gms.ads.MobileAds;
+public class AIDEApplication extends Application
+{
+	private Thread.UncaughtExceptionHandler uncaughtExceptionHandler;
