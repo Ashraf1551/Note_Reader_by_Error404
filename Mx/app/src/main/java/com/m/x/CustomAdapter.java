3339a969c35f13cd76451cf7708944ae2d8e20e2
Rian49 <alam23105101563@diu.edu.bd>
2024-12-03 22:29:38 +0600

    fourth commit

---
+++ Mx/app/src/main/java/com/m/x/CustomAdapter.java
@@ -0,0 +1,44 @@
+package com.m.x;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import android.widget.*;
+
+public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHolder> {
+
+    private Context context;
+    private Activity activity;
+    private ArrayList book_id, book_title, book_author;
+
+    CustomAdapter(Activity activity, Context context, ArrayList book_id, ArrayList book_title, ArrayList book_author){
+        this.activity = activity;
+        this.context = context;
+        this.book_id = book_id;
+        this.book_title = book_title;
+        this.book_author = book_author;
+        //this.book_pages = book_pages;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.my_row, parent, false);
+        return new MyViewHolder(view);
+    }
