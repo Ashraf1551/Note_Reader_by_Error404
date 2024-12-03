c0f056bcd52e97ffaa5d7721b40e2d1266d82171
Rian49 <alam23105101563@diu.edu.bd>
2024-12-03 23:45:05 +0600

    fifth commit

--- Mx/app/src/main/java/com/m/x/CustomAdapter.java
+++ Mx/app/src/main/java/com/m/x/CustomAdapter.java
@@ -42,3 +42,53 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHold
         View view = inflater.inflate(R.layout.my_row, parent, false);
         return new MyViewHolder(view);
     }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void onBindViewHolder(@NonNull final MyViewHolder holder, final int position) {
+        //holder.book_id_txt.setText(String.valueOf(book_id.get(position)));
+        holder.book_title_txt.setText(String.valueOf(book_title.get(position)));
+        holder.book_author_txt.setText(String.valueOf(book_author.get(position)));
+        //holder.book_pages_txt.setText(String.valueOf(book_pages.get(position)));
+        //Recyclerview onClickListener
+        holder.mainLayout.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View view) {
+					Intent intent = new Intent(context, ReadActivity.class);
+					intent.putExtra("id", String.valueOf(book_id.get(position)));
+					intent.putExtra("title", String.valueOf(book_title.get(position)));
+					intent.putExtra("author", String.valueOf(book_author.get(position)));
+					//intent.putExtra("pages", String.valueOf(book_pages.get(position)));
+					activity.startActivityForResult(intent, 1);
+				}
+			});
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return book_id.size();
+    }
+
+    class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView book_title_txt, book_author_txt, book_pages_txt;
+		ImageView book_id_txt;
+        LinearLayout mainLayout;
+
+        MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            book_id_txt = itemView.findViewById(R.id.book_id);
+            book_title_txt = itemView.findViewById(R.id.book_title_txt);
+            book_author_txt = itemView.findViewById(R.id.book_author_txt);
+            //book_pages_txt = itemView.findViewById(R.id.book_pages_txt);
+            mainLayout = itemView.findViewById(R.id.mainLayout);
+            //Animate Recyclerview
+            Animation translate_anim = AnimationUtils.loadAnimation(context, R.anim.translate_anim);
+            mainLayout.setAnimation(translate_anim);
+        }
+
+    }
+
+}
