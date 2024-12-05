f1eb5ebe826092f5e18fa6016eb2fd08806b8ba3
Mahadi1828 <hassan2305101828@diu.edu.bd>
2024-12-05 23:17:39 +0600

    28th commit

--- Mx/app/src/main/java/com/m/x/AddActivity.java
+++ Mx/app/src/main/java/com/m/x/AddActivity.java
@@ -47,3 +47,11 @@ public boolean onCreateOptionsMenu(Menu menu) {
     getMenuInflater().inflate(R.menu.add_menu, menu);
     return true;
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+
