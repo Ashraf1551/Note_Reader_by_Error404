61233ae74c6bad8bc31676d22f23ade370986122
Mahadi1828 <hassan2305101828@diu.edu.bd>
2024-12-05 22:23:25 +0600

    23th commit

--- Mx/app/src/main/java/com/m/x/AddActivity.java
+++ Mx/app/src/main/java/com/m/x/AddActivity.java
@@ -17,3 +17,8 @@ public class AddActivity extends AppCompatActivity {
 	private Toolbar toolbar;
     EditText title_input, author_input;
 	InterstitialAd iad;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
