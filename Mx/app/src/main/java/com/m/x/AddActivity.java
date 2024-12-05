516f1a60978abe4ca4475824e0a97d0bc018a603
Mahadi1828 <hassan2305101828@diu.edu.bd>
2024-12-05 22:07:38 +0600

    21th commit

--- Mx/app/src/main/java/com/m/x/AddActivity.java
+++ Mx/app/src/main/java/com/m/x/AddActivity.java
@@ -1,3 +1,12 @@
 package com.m.x;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.appcompat.widget.*;
+import android.content.*;
+import android.view.*;
+import com.google.android.gms.ads.*;
