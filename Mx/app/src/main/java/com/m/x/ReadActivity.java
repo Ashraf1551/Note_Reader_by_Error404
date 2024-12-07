79d768a8c9a3d5207ab101c6eb483cc9d8c26025
Mahadi1828 <hassan2305101828@diu.edu.bd>
2024-12-07 20:55:16 +0600

    33th commit

--- Mx/app/src/main/java/com/m/x/ReadActivity.java
+++ Mx/app/src/main/java/com/m/x/ReadActivity.java
@@ -3,3 +3,20 @@ package com.m.x;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.appcompat.widget.*;
+import android.view.*;
+import android.speech.tts.*;
+import java.util.*;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import com.google.android.gms.ads.*;
+
