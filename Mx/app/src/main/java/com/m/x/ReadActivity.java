3e430de4e9c029aa51a2f1aa9e96bd3ae8aebc09
Mahadi1828 <hassan2305101828@diu.edu.bd>
2024-12-08 01:25:52 +0600

    48 th commit

--- Mx/app/src/main/java/com/m/x/ReadActivity.java
+++ Mx/app/src/main/java/com/m/x/ReadActivity.java
@@ -148,3 +148,11 @@ void getAndSetIntentData(){
 			});
         builder.create().show();
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.update_menu, menu);
+        return true;
+    }
