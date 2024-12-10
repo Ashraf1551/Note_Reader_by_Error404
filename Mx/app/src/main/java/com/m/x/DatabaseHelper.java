b1f011c8ff46a50a9cad526bce8ef02dda6f15f4
Ashraf1551 <mdashrafgmhs@gmail.com>
2024-12-10 21:43:15 +0600

    Defining the class and constructor

---
+++ Mx/app/src/main/java/com/m/x/DatabaseHelper.java
@@ -0,0 +1,28 @@
+package com.m.x;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+import android.widget.Toast;
+import androidx.annotation.Nullable;
+
+class DatabaseHelper extends SQLiteOpenHelper {
+
+    private Context context;
+    private static final String DATABASE_NAME = "BookLibrary.db";
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String TABLE_NAME = "my_library";
+    private static final String COLUMN_ID = "_id";
+    private static final String COLUMN_TITLE = "book_title";
+    private static final String COLUMN_AUTHOR = "book_author";
+    //private static final String COLUMN_PAGES = "book_pages";
+
+    DatabaseHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        this.context = context;
+    }
+}
