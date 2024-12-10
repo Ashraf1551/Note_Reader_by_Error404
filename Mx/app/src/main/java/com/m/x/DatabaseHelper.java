a3a1cc6c9d3f1a99114f53075fe2744e714b95be
Ashraf1551 <mdashrafgmhs@gmail.com>
2024-12-10 22:48:19 +0600

    create & upgrade

--- Mx/app/src/main/java/com/m/x/DatabaseHelper.java
+++ Mx/app/src/main/java/com/m/x/DatabaseHelper.java
@@ -25,4 +25,19 @@ class DatabaseHelper extends SQLiteOpenHelper {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
         this.context = context;
     }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String query = "CREATE TABLE " + TABLE_NAME +
+                        " (" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                        COLUMN_TITLE + " TEXT, " +
+                        COLUMN_AUTHOR + " TEXT);";
+        db.execSQL(query);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
 }
