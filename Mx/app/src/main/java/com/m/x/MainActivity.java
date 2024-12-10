685ead5c1ac8d71bd0e6e4d71aabbd3a57f206a2
Sami_1582 <sami23105101582@diu.edu.bd>
2024-12-10 22:50:42 +0600

    20th commit

--- Mx/app/src/main/java/com/m/x/MainActivity.java
+++ Mx/app/src/main/java/com/m/x/MainActivity.java
@@ -1,3 +1,31 @@
+package com.m.x;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import com.google.android.gms.ads.*;
+import androidx.appcompat.widget.*;
+
 public class MainActivity extends AppCompatActivity {
 
 	AdView bad;
