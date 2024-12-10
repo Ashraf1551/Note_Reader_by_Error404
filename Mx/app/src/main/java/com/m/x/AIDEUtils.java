ef6e425ba1ea841c28bb066af618e3f85fd03e04
Sami_1582 <sami23105101582@diu.edu.bd>
2024-12-10 21:22:17 +0600

    11th commit

--- Mx/app/src/main/java/com/m/x/AIDEUtils.java
+++ Mx/app/src/main/java/com/m/x/AIDEUtils.java
@@ -66,3 +66,19 @@ public class AIDEUtils {
 		double t = ((((size/1024.0)/1024.0)/1024.0)/1024.0);
 
 		DecimalFormat dec = new DecimalFormat("0.00");
+        if ( t>1 ) {
+			hrSize = dec.format(t).concat(" TB");
+		} else if ( g>1 ) {
+			hrSize = dec.format(g).concat(" GB");
+		} else if ( m>1 ) {
+			hrSize = dec.format(m).concat(" MB");
+		} else if ( k>1 ) {
+			hrSize = dec.format(k).concat(" KB");
+		} else {
+			hrSize = dec.format(b).concat(" Bytes");
+		}
+
+		return hrSize;
+		
+	}
+}
