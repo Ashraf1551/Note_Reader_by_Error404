97d0f2d8491af9a8813b0271cf649556b6fdd375
easin1740 <hossan2305101740@diu,edu.bd>
2024-12-10 11:38:46 +0600

    2nd commit on AIDEApplication

--- Mx/app/src/main/java/com/m/x/AIDEApplication.java
+++ Mx/app/src/main/java/com/m/x/AIDEApplication.java
@@ -15,3 +15,30 @@ public class AIDEApplication extends Application
 
 		Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
 			@Override
+			public void onCreate() {
+				MobileAds.initialize(this, "ca-app-pub-3940256099942544~3347511713");
+				this.uncaughtExceptionHandler = Thread.getDefaultUncaughtExceptionHandler();
+		
+				Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+					@Override
+					public void uncaughtException(Thread thread, Throwable ex) {
+						Intent intent = new Intent(getApplicationContext(), DebugActivity.class);
+						intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+		
+						intent.putExtra("error", getStackTrace(ex));
+		
+						PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 11111, intent, PendingIntent.FLAG_ONE_SHOT);
+		
+		
+						AlarmManager am = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+						am.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, 1000, pendingIntent);
+		
+						android.os.Process.killProcess(android.os.Process.myPid());
+						System.exit(2);
+		
+						uncaughtExceptionHandler.uncaughtException(thread, ex);
+					}
+				});
+				super.onCreate();
+		
+			}
