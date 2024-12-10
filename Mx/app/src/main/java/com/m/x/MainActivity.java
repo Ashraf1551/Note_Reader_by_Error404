27fbe2bf92cefa0a5bcd26be73322e9c5e3c6d5d
Sami_1582 <sami23105101582@diu.edu.bd>
2024-12-10 22:46:27 +0600

    15th commit

--- Mx/app/src/main/java/com/m/x/MainActivity.java
+++ Mx/app/src/main/java/com/m/x/MainActivity.java
@@ -46,3 +46,10 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
 
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1){
+            recreate();
+        }
+    }
