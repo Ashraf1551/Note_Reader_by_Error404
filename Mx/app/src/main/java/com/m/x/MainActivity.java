ea18ff8c688a65cb83543c047cb4e1336fc8439b
Sami_1582 <sami23105101582@diu.edu.bd>
2024-12-10 22:44:08 +0600

    12th commit

--- Mx/app/src/main/java/com/m/x/MainActivity.java
+++ Mx/app/src/main/java/com/m/x/MainActivity.java
@@ -9,3 +9,17 @@ public class MainActivity extends AppCompatActivity {
     DatabaseHelper myDB;
     ArrayList<String> book_id, book_title, book_author;
     CustomAdapter customAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+		
+		Toolbar toolbar = findViewById(R.id.mainToolbar);
+		setSupportActionBar(toolbar);
+		getSupportActionBar().setTitle("Note Reader");
+		
+		MobileAds.initialize(this, "ca-app-pub-3940256099942544~3347511713");
+
+		bad=findViewById(R.id.adView);
+		AdRequest adRequest = new AdRequest.Builder().build();
+		bad.loadAd(adRequest);
